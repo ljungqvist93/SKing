a21035d5270149a792f75b95a2b13eb6b4873001
ljungqvist93 <ljungqvist1993@gmail.com>
2020-01-19 23:05:09 +0100

    Fixed theme.

--- assets/parts/head.php
+++ assets/parts/head.php
@@ -1,3 +1,33 @@
+<?php 
+
+
+    $themes = [
+        'light',
+        'dark',
+    ];
+    $theme = null;
+    if(isset($_COOKIE['theme'])) {
+        $theme = in_array($_COOKIE['theme'], $themes) ? $_COOKIE['theme'] : 'light';
+    }
+    if(isset($_GET['theme'])) {
+        $location = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+        $params = $_GET;
+        unset($params['theme']);
+        if(count($params)) {
+            $location .= '?'.http_build_query($params);
+        }
+        if($_GET['theme'] === 'auto') {
+            setcookie('theme', null, -1);
+            header('Location: '.$location);
+            die();
+        }
+        $theme = in_array($_GET['theme'], $themes) ? $_GET['theme'] : 'light';
+        setcookie('theme', $theme, intval(time()+60*60*24*30*360));
+        header('Location: '.$location);
+        die();
+    }
+
+ ?>
 <head>
 	<meta charset="UTF-8">
 	<title>Daniel Ljungqvist</title>
@@ -8,4 +38,41 @@
     <meta data-n-head="true" name="viewport" content="width=device-width, initial-scale=1"/>
 
     <meta name="description" content="Daniel Ljungqvist provides really rad, easy and interesting guides and articles about technology, IT, and much more!" />
+
+
+    <script>
+        function getCookie(cname) {
+            var name = cname + "=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for(var i = 0; i <ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+        if(window.matchMedia('(prefers-color-scheme').media === 'not all' && getCookie('theme') !== 'dark') {
+            var head = document.getElementsByTagName('HEAD')[0];
+            var link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = '/assets/style/lightstyle.css';
+            head.appendChild(link);
+        }
+    </script>
+
+
+    <?php if($theme === 'light'): ?>
+        <link rel="stylesheet" href="/assets/style/lightstyle.css" type="text/css" />
+    <?php elseif($theme === 'dark'): ?>
+        <link rel="stylesheet" href="/assets/style/darkstyle.css" type="text/css" />
+    <?php elseif(!$theme): ?>
+        <link rel="stylesheet" href="/assets/style/lightstyle.css" type="text/css" media="(prefers-color-scheme: no-preference), (prefers-color-scheme: light)" />
+        <link rel="stylesheet" href="/assets/style/darkstyle.css" type="text/css" media="(prefers-color-scheme: dark)" />
+    <?php endif; ?>
 </head>
--- index.php
+++ index.php
@@ -18,6 +18,19 @@
                     <li>About me</li>
                 </ul>
             </nav>
+            <div id="themeButton">
+                <?php if(is_theme('light')): ?>
+                    <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
+                        <i class="fal fa-toggle-off"></i>
+                        Dark
+                    </a>
+                <?php else: ?>
+                    <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
+                    <i class="fal fa-toggle-on"></i>
+                        Dark
+                    </a>
+                <?php endif; ?>
+            </div>
             <h1>Daniel Ljungqvist</h1>
         </section>
         <div class="wrapper">
