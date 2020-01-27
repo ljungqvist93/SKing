61ac3fbaf2c68c70ce684d515cdba9ef49d0d3ba
ljungqvist93 <ljungqvist1993@gmail.com>
2020-01-27 11:55:10 +0100

    Mobile menu in place.

--- assets/js.js
+++ assets/js.js
@@ -5,3 +5,8 @@ $('#body .large').parent('p').addClass('llarge');
 $('#toggleAdmin').on('click', function () {
 	$('#adminTools').toggle();
 });
+
+
+$('.fa-bars').click(function () {
+	$('#mobileMenu').slideToggle('fast');
+});
--- assets/parts/topper.php
+++ assets/parts/topper.php
@@ -23,3 +23,23 @@
             <h1>Daniel Ljungqvist</h1>
             <i class="far fa-bars"></i>
         </section>
+        <div id="mobileMenu">
+            <ul>
+                <li><a href="articles.php">Articles</a></li>
+                <li><a href="guides.php">Guides</a></li>
+                <li><a href="about.php">About me</a></li>
+            </ul>
+            <div id="themeButton">
+                <?php if(is_theme('light')): ?>
+                    <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
+                        <i class="fal fa-toggle-off"></i>
+                        Dark
+                    </a>
+                <?php else: ?>
+                    <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
+                    <i class="fal fa-toggle-on"></i>
+                        Dark
+                    </a>
+                <?php endif; ?>
+            </div>
+        </div>
