0cc2eddd7df847f0aa86375e46d8a58985e69086
ljungqvist93 <ljungqvist1993@gmail.com>
2020-01-29 22:48:54 +0100

    Startpage update begun...

--- assets/parts/topper.php
+++ assets/parts/topper.php
@@ -1,45 +1,48 @@
-
-        <section id="topper">
-            <nav id="main">
-                <ul class="inline">
-                    <li>Articles</li>
-                    <li>Guides</li>
-                    <li>About me</li>
-                </ul>
-            </nav>
-            <div id="themeButton">
-                <?php if(is_theme('light')): ?>
-                    <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
-                        <i class="fal fa-toggle-off"></i>
-                        Dark
-                    </a>
-                <?php else: ?>
-                    <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
-                    <i class="fal fa-toggle-on"></i>
-                        Dark
-                    </a>
-                <?php endif; ?>
-            </div>
-            <h1>Daniel Ljungqvist</h1>
-            <i class="far fa-bars"></i>
-        </section>
-        <div id="mobileMenu">
-            <ul>
-                <li><a href="articles.php">Articles</a></li>
-                <li><a href="guides.php">Guides</a></li>
-                <li><a href="about.php">About me</a></li>
-            </ul>
-            <div id="themeButton">
-                <?php if(is_theme('light')): ?>
-                    <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
-                        <i class="fal fa-toggle-off"></i>
-                        Dark
-                    </a>
-                <?php else: ?>
-                    <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
-                    <i class="fal fa-toggle-on"></i>
-                        Dark
-                    </a>
-                <?php endif; ?>
-            </div>
+<section id="topper">
+    <div id="bg">
+        <header>
+            <h1>CYBERLAD</h1>
+            <p>Alright, alright, alright, you gonna learn today!</p>
+        </header>
+    </div>
+    <nav>
+        <ul class="inline">
+            <li><a href="articles.php">Articles</a></li>
+            <li><a href="guides.php">Guides</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+        <div id="themeButton">
+            <?php if(is_theme('light')): ?>
+                <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
+                    <i class="fal fa-toggle-off"></i>
+                    Dark
+                </a>
+            <?php else: ?>
+                <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
+                <i class="fal fa-toggle-on"></i>
+                    Dark
+                </a>
+            <?php endif; ?>
         </div>
+    </nav>
+</section>
+<div id="mobileMenu">
+    <ul>
+        <li><a href="articles.php">Articles</a></li>
+        <li><a href="guides.php">Guides</a></li>
+        <li><a href="about.php">About me</a></li>
+    </ul>
+    <div id="themeButton">
+        <?php if(is_theme('light')): ?>
+            <a href="<?= build_theme_link('dark'); ?>" class="<?= is_theme('dark') ? 'themeselected' : '' ?>">
+                <i class="fal fa-toggle-off"></i>
+                Dark
+            </a>
+        <?php else: ?>
+            <a href="<?= build_theme_link('light'); ?>" class="<?= is_theme('light') ? 'themeselected' : '' ?>">
+            <i class="fal fa-toggle-on"></i>
+                Dark
+            </a>
+        <?php endif; ?>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,11 +7,44 @@
     ")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
+<?php include 'assets/parts/fonts.php'; ?>
 <html lang="en">
 	<?php include 'assets/parts/head.php'; ?>
 	<body id="startPage">
         <?php include 'assets/parts/topper.php'; ?>
-        <div class="wrapper">
+        <ul id="cinema" class="inline">
+            <li>
+                <picture>
+                    <source srcset="http://cyberlad.com/engine/image/eMGAZvz4bFK7B9iP1kaxy6joX.jpg" media="(max-width:660px)">
+                    <source srcset="http://cyberlad.com/engine/image/g5R03ZdQoKBPvAs2LFfiuz6qp.jpg" media="(min-width:661px)">
+                    <img src="http://cyberlad.com/engine/image/eMGAZvz4bFK7B9iP1kaxy6joX.jpg" alt="">
+                </picture>
+                <header>
+                    <h3>Lorem ipsum dolor sit amet.</h3>
+                </header>
+            </li>
+            <li>
+                <picture>
+                    <source srcset="http://cyberlad.com/engine/image/UvbJCuyhRXtGHWiekLDV70IE9.jpg" media="(max-width:660px)">
+                    <source srcset="http://cyberlad.com/engine/image/GhOM8TEyVvqHkUrsPQgXxYmK9.jpg" media="(min-width:661px)">
+                    <img src="http://cyberlad.com/engine/image/UvbJCuyhRXtGHWiekLDV70IE9.jpg" alt="">
+                </picture>
+                <header>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                </header>
+            </li>
+            <li>
+                <picture>
+                    <source srcset="" media="">
+                    <source srcset="" media="">
+                    <img src="" alt="">
+                </picture>
+                <header>
+                    <h3>Lorem ipsum dolor sit amet, consectetur.</h3>
+                </header>
+            </li>
+        </ul>
+        <div class="wrapper" style="display: none;">
             <?php foreach ($posts as $post): ?>
                 <article>
                     <a href="post.php?slug=<?= $post['slug']; ?>">
@@ -21,7 +54,6 @@
                             <img src="<?= $post['thumbImage']; ?>" alt="">
                         </picture>
                         <h2><?= $post['title']; ?></h2>
-                        <?= $post['id']; ?>
                     </a>
                 </article>
             <?php endforeach; ?>
